071da649fb48db8667ef49c0ed3ed15e20a2d5d4
funmi7 <talabifunmilayo@gmail.com>
2020-02-28 09:58:32 +0100

    completed the ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,34 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            if self.current == None:
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+                self.current = self.storage.head
+
+            elif self.current.next:
+                self.current.insert_after(item)
+                self.current = self.current.next
+                self.current.next.delete()
+            else:
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+                self.current = self.storage.head
+
+                
+        else:
+            self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        head = self.storage.head
+        while head:
+            list_buffer_contents.append(head.value)
+            head = head.next
 
         return list_buffer_contents
 
